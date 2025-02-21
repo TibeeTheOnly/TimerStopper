ff0cbfbeabc91ed4b7f5081080ae14ad8f7d20a5
Tibor Hornyák <hornyak.tibor@petrik.hu>
2025-02-21 13:48:35 +0100

    feat: stopper

--- TimerStopper/StopperWindow.xaml.cs
+++ TimerStopper/StopperWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace TimerStopper
 {
@@ -19,9 +21,53 @@ namespace TimerStopper
     /// </summary>
     public partial class StopperWindow : Window
     {
+        private DispatcherTimer StopperTimer;
+
+        private TimeSpan ElapsedTime;
+
         public StopperWindow()
         {
             InitializeComponent();
+
+            ElapsedTime = TimeSpan.Zero;
+
+            StopperTimer = new DispatcherTimer()
+            {
+                Interval = TimeSpan.FromMilliseconds(1)
+            };
+
+            StopperTimer.Tick += StopperTimer_Tick;
+        }
+
+        private void StopperTimer_Tick(object? sender, EventArgs e)
+        {
+            //eltelt idő növelése 1000ms-el
+            ElapsedTime = ElapsedTime.Add(TimeSpan.FromMilliseconds(1000));
+
+            //megjelenítés frissítése
+            UpdateDisplay();
+        }
+
+        private void UpdateDisplay()
+        {
+            StopperDisplay.Text = ElapsedTime.ToString();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            StopperTimer.Start();
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            StopperTimer.Stop();
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            StopperTimer.Stop();
+            ElapsedTime = TimeSpan.Zero;
+            UpdateDisplay();
         }
     }
 }
